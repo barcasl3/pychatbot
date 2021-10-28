470b50dd7bc0e415cb5a66ac2bc890d1d6625224
barcasl3 <barcaasl3@gmail.com>
2021-10-28 15:16:38 +0200

    emmorphpy, purepospy

--- pychatbot/bot.py
+++ pychatbot/bot.py
@@ -4,14 +4,30 @@
 from botbuilder.core import ActivityHandler, TurnContext
 from botbuilder.schema import ChannelAccount
 from quntoken import tokenize
+from emmorphpy import EmMorphPy
+from purepospy import PurePOS
+
+m = EmMorphPy()
+p = PurePOS('/home/barcasl3/Downloads/emmorphpy-master/purepospy/purepospy/szeged.model')
 
 
 class MyBot(ActivityHandler):
     # See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
 
     async def on_message_activity(self, turn_context: TurnContext):
+        szavak = []
+        i = 0
         for tok in tokenize(turn_context.activity.text):
-            print(tok, end='')
+            szavak.append(tok.split('\t')[0])
+            i += 1
+        
+        tokenized = ' '.join(szavak)
+        print(tokenized)
+
+        tagged = p.tag_sentence(tokenized)
+        print(list(tagged))
+            
+        
         await turn_context.send_activity(f"You said '{ turn_context.activity.text }'")
 
     async def on_members_added_activity(
